d0bb9afb099376fe7fb6aac6d6f45fd40e40d465
Konstantin Krivopustov <krivopustov@haulmont.com>
2025-11-01 15:46:47 +0400

    Temporary fix of jmix-framework/jmix#4850

---
+++ frontend-app/src/main/java/com/company/frontend/FrontendRestDsEntityConfigurer.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.company.frontend;
+
+import com.google.common.base.Strings;
+import io.jmix.core.JmixOrder;
+import io.jmix.core.MetadataMutationTools;
+import io.jmix.core.MetadataPostProcessor;
+import io.jmix.core.metamodel.model.Session;
+import io.jmix.flowuirestds.genericfilter.FilterConfiguration;
+import io.jmix.flowuirestds.settings.UserSettingsItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Temporary fix of https://github.com/jmix-framework/jmix/issues/4850
+ */
+@Component
+@Order(JmixOrder.LOWEST_PRECEDENCE - 100)
+public class FrontendRestDsEntityConfigurer implements MetadataPostProcessor {
+
+    @Value("${jmix.restds.ui-config-store:}")
+    private String uiConfigStore;
+
+    @Autowired
+    private MetadataMutationTools metadataMutationTools;
+
+    @Override
+    public void process(Session session) {
+        if (Strings.isNullOrEmpty(uiConfigStore))
+            return;
+
+        metadataMutationTools.setStore(session.getClass(UserSettingsItem.class), uiConfigStore);
+        metadataMutationTools.setStore(session.getClass(FilterConfiguration.class), uiConfigStore);
+    }
+}
